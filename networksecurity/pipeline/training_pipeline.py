ebdf3b9bf21d19dca0bcb6f8350283b02d49f3ed
Efosa <efoa_wil@yahoo.com>
2024-08-24 11:37:42 +0100

    config entity

---
+++ networksecurity/pipeline/training_pipeline.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.components.model_evaluation import ModelEvaluation
+from networksecurity.components.model_pusher import ModelPusher
+
+from networksecurity.entity.config_entity import (
+    TrainingPipelineConfig,
+    DataIngestionConfig,
+    DataValidationConfig,
+    DataTransformationConfig,
+    ModelTrainerConfig,
+    ModelEvaluationConfig,
+    ModelPusherConfig,
+
+)
+
+from networksecurity.entity.artifact_entity import (
+    DataIngesstionArtifact,
+    DataValidationArtifact,
+    DataTransformationArtifact,
+    ModelTrainerArtifact,
+    ModelEvaluationArtifact,
+    ModelPusherArtifact
+
+)
+
+
+class TrainingPipeline:
+    def __init__(self):
+        self.training_pipeline_config = TrainingPipelineConfig()
