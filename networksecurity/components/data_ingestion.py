f569b3fe55d1e41b3ad497fbbe83947ac9f5bb47
Efosa <efoa_wil@yahoo.com>
2024-08-28 14:59:42 +0100

    ingestion completed

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -86,8 +86,8 @@ class DataIngestion:
             dataframe = self.export_data_into_feature_store(dataframe)
             self.split_data_as_train_test(dataframe=dataframe)
 
-            data_ingestion_artifact = DataIngestionArtifact(trained_file_path=self.data_ingestion_config.trainging_file_path,
-                                                            test_file_path=self.data_ingestion_config.test_file_path)
+            data_ingestion_artifact = DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
+                                                            test_file_path=self.data_ingestion_config.testing_file_path)
             return data_ingestion_artifact
 
         except Exception as e:
--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -23,7 +23,7 @@ from networksecurity.entity.config_entity import (
 )
 
 from networksecurity.entity.artifact_entity import (
-    DataIngesstionArtifact,
+    DataIngestionArtifact,
     DataValidationArtifact,
     DataTransformationArtifact,
     ModelTrainerArtifact,
@@ -39,7 +39,16 @@ class TrainingPipeline:
 
     def start_data_ingestion(self):
         try:
-            pass
+            self.data_ingestion_config = DataIngestionConfig(
+                training_pipeline_config=self.training_pipeline_config)
+            logging.info("Starting data ingestion")
+            data_ingestion = DataIngestion(
+                data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info(
+                f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
+
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
@@ -75,6 +84,7 @@ class TrainingPipeline:
 
     def run_pipeline(self):
         try:
-            pass
+            data_ingestion_artifact = self.start_data_ingestion()
+
         except Exception as e:
             raise NetworkSecurityException(e, sys)
